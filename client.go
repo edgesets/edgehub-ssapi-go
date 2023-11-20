1ed588e0fffd8037ee22cc7605d2eaf17ff85b17
liqiulin <liqiulin@outlook.com>
2023-11-20 15:32:15 +0800

    update

--- client.go
+++ client.go
@@ -1,17 +1,24 @@
 package ssapigo
 
 import (
+	"context"
+	"fmt"
+	sharedpb "github.com/edgesets/edgehub-protocol/shared"
 	ssapiv1 "github.com/edgesets/edgehub-protocol/ssapi/v1"
+	"github.com/golang/protobuf/proto"
+	"github.com/google/uuid"
+	"go.uber.org/multierr"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Client struct {
 	ssapiv1.ServerSideApiClient
+	timeoutMs int32
 }
 
 func NewClient(url string, withSSL bool) (*Client, error) {
-	opts := []grpc.DialOption{}
+	var opts []grpc.DialOption
 	if !withSSL {
 		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	}
@@ -22,5 +29,42 @@ func NewClient(url string, withSSL bool) (*Client, error) {
 	ac := ssapiv1.NewServerSideApiClient(cc)
 	return &Client{
 		ServerSideApiClient: ac,
+		timeoutMs:           5_000,
 	}, nil
+}
+
+type ReplyDecodeFn func(payload []byte) (proto.Message, error)
+
+func (c *Client) Survey(ctx context.Context, topic string, route string, req proto.Message, waitReplies int, decodeFn ReplyDecodeFn) ([]proto.Message, error) {
+	bs, err := proto.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	rep, err := c.ServerSideApiClient.Survey(ctx, &ssapiv1.SurveyRequest{
+		Id:           uuid.NewString(),
+		WaitReplies:  uint32(waitReplies),
+		Timeout:      uint32(c.timeoutMs), // millis
+		Topic:        topic,
+		Route:        route,
+		ContentType:  sharedpb.ContentType_PROTOBUF.String(),
+		PayloadBytes: bs,
+	})
+	if e := rep.Error; e != nil {
+		return nil, fmt.Errorf("[%d] %s", e.Code, e.Message)
+	}
+	var errs error
+	var replies []proto.Message
+	for _, res := range rep.Results {
+		if e := res.Error; e != nil {
+			errs = multierr.Append(errs, fmt.Errorf("[%d] %s", e.Code, e.Message))
+			continue
+		}
+
+		// TODO
+
+		//reply, e := decodeFn(res)
+
+	}
+	return replies, err
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/edgesets/edgehub-ssapi-go
 go 1.21.1
 
 require (
-	github.com/edgesets/edgehub-protocol v0.0.6
+	github.com/edgesets/edgehub-protocol v0.0.7
+	github.com/golang/protobuf v1.5.3
 	github.com/google/uuid v1.3.1
 	github.com/stretchr/testify v1.8.4
 	google.golang.org/grpc v1.59.0
@@ -14,10 +15,10 @@ require (
 	github.com/go-kratos/aegis v0.2.0 // indirect
 	github.com/go-kratos/kratos/v2 v2.7.1 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
