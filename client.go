85268a26cd44a921f53223e2995dfee9cdf8dd0d
liqiulin <liqiulin@outlook.com>
2023-11-24 18:11:53 +0800

    update

--- client.go
+++ client.go
@@ -2,12 +2,10 @@ package ssapigo
 
 import (
 	"context"
-	"fmt"
 	sharedpb "github.com/edgesets/edgehub-protocol/shared"
 	ssapiv1 "github.com/edgesets/edgehub-protocol/ssapi/v1"
 	"github.com/golang/protobuf/proto"
 	"github.com/google/uuid"
-	"go.uber.org/multierr"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -35,13 +33,13 @@ func NewClient(url string, withSSL bool) (*Client, error) {
 
 type ReplyDecodeFn func(payload []byte) (proto.Message, error)
 
-func (c *Client) Survey(ctx context.Context, topic string, route string, req proto.Message, waitReplies int, decodeFn ReplyDecodeFn) ([]proto.Message, error) {
+func (c *Client) Survey(ctx context.Context, topic string, route string, req proto.Message, waitReplies int) (*ssapiv1.SurveyReply, error) {
 	bs, err := proto.Marshal(req)
 	if err != nil {
 		return nil, err
 	}
 
-	rep, err := c.ServerSideApiClient.Survey(ctx, &ssapiv1.SurveyRequest{
+	return c.ServerSideApiClient.Survey(ctx, &ssapiv1.SurveyRequest{
 		Id:           uuid.NewString(),
 		WaitReplies:  uint32(waitReplies),
 		Timeout:      uint32(c.timeoutMs), // millis
@@ -50,21 +48,4 @@ func (c *Client) Survey(ctx context.Context, topic string, route string, req pro
 		ContentType:  sharedpb.ContentType_PROTOBUF.String(),
 		PayloadBytes: bs,
 	})
-	if e := rep.Error; e != nil {
-		return nil, fmt.Errorf("[%d] %s", e.Code, e.Message)
-	}
-	var errs error
-	var replies []proto.Message
-	for _, res := range rep.Results {
-		if e := res.Error; e != nil {
-			errs = multierr.Append(errs, fmt.Errorf("[%d] %s", e.Code, e.Message))
-			continue
-		}
-
-		// TODO
-
-		//reply, e := decodeFn(res)
-
-	}
-	return replies, err
 }
